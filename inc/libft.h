e0c266ea1ff60049a0770e1d28e8033220d3c09e
Monika Tuominen <motuomin@c1r5p9.hive.fi>
2024-06-12 16:10:36 +0300

    Pimped up libft.h

--- inc/libft.h
+++ inc/libft.h
@@ -6,15 +6,23 @@
 /*   By: motuomin <motuomin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:39:31 by motuomin          #+#    #+#             */
-/*   Updated: 2024/06/12 14:30:14 by motuomin         ###   ########.fr       */
+/*   Updated: 2024/06/12 16:10:12 by motuomin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H 
 
+// ╔════════════════════════════════╗
+// ║		    Includes	        ║
+// ╚════════════════════════════════╝
+
 # include <stddef.h>
 # include "ft_printf.h"
+
+// ╔════════════════════════════════╗
+// ║         Type Definitions       ║
+// ╚════════════════════════════════╝
 
 typedef struct s_list
 {
@@ -22,34 +30,42 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-// --- Function prototypes ---
+// ╔════════════════════════════════╗
+// ║       Function Prototypes      ║
+// ╚════════════════════════════════╝
 
-// Standard library
+//		Character functions
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+//		String functions
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strdup(const char *s1);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+//		Memory functions
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *s1);
 
-// Additional functions
+//		Conversion functions
+int		ft_atoi(const char *str);
+
+//		 Additional functions
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -62,7 +78,7 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-// Bonus functions
+//		Linked list functions
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
