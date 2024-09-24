b4b9df5ab2c3afe6736cd9333caab427d6a9c749
Monika <monsulit@gmail.com>
2024-09-24 23:48:39 +0300

    Added ft_memdel.c

--- inc/libft.h
+++ inc/libft.h
@@ -55,6 +55,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+void	*ft_memdel(char **ptr);
 
 //		Conversion functions
 int		ft_atoi(const char *str);
